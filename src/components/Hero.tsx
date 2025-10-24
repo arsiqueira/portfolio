befb398e2f392296411829be91e91fa08bf61f00
arsiqueira <125691224+arsiqueira@users.noreply.github.com>
2025-10-24 12:29:39 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ const Hero = () => {
             Desenvolvendo experiências digitais que unem <span className="font-semibold text-primary">criatividade, empatia e propósito</span>
           </p>
 
-     <img
+    <img
   src="ananda-oi.png"
   alt="Decoração"
   id="decoracao"
@@ -62,10 +62,8 @@ const Hero = () => {
     opacity-60 sm:opacity-75 md:opacity-90
     pointer-events-none
     transition-all duration-700 ease-in-out
-    translate-x-7 sm:translate-x-9 md:translate-x-11 lg:translate-x-13 xl:translate-x-15"
+    -translate-x-7 sm:-translate-x-9 md:-translate-x-11 lg:-translate-x-13 xl:-translate-x-15"
 />
-
-
 
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center items-center pt-4">
